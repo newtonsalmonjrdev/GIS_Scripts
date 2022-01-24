1647b4e4723fdf1a886ab144cc08c689ab0ec108
newtonsalmonjrdev <82189102+newtonsalmonjrdev@users.noreply.github.com>
2022-01-23 19:04:45 -0500

    Add files via upload
    A simple script to format the imported latitude coordinates from the Forestry Tree Points .csv file found on NYC Open Data.

---
+++ Forestry_Trees_Coord_lat.py
@@ -0,0 +1,12 @@
+# open both files
+with open('Foresty_coord_csv_auto_txt.txt', 'r') as firstfile, open('Foresty_coord_lat.txt', 'w') as secondfile:
+    # read content from first file
+    for line in firstfile:
+        if len(line.strip()) == 0:
+            continue
+        else:
+            line_format = line.replace("(", "").replace(")", "").replace(",", "").replace("\"","")
+            line_format = line_format.split(" ")
+            # append content to second file
+            secondfile.write(line_format[0] + "\n")
+    secondfile.flush()
