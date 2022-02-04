7c9296a7220b7941133a4accf29dc2d94436e695
newtonsalmonjrdev <82189102+newtonsalmonjrdev@users.noreply.github.com>
2022-02-03 21:39:23 -0500

    Update Forestry_Trees_Coord_long.py

--- Forestry_Trees_Coord_long.py
+++ Forestry_Trees_Coord_long.py
@@ -1,3 +1,5 @@
+#code to process longitudinal coordinates into ArcMap/ArcPro
+
 # open both files
 with open('Foresty_coord_csv_auto_txt.txt', 'r') as firstfile, open('Forestry_coord_long.txt', 'w+') as secondfile:
     # read content from first file
